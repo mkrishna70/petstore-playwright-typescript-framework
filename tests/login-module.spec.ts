36ffeb46ceaf6d310ef2100f286e98192c0df9d2
mkrishna70 <mallam.krishna70@gmail.com>
2026-08-01 06:37:49 -0400

    committed

--- tests/login-module.spec.ts
+++ tests/login-module.spec.ts
@@ -1,8 +1,5 @@
 import {test} from '../fixtures/hooks-fixture'
 
-/**
- * TESTing
- */
 test('Login',async({page,gotoURL,loginPage})=>{
     await loginPage.enterUserCredentials(`${process.env.USER_NAME}`,`${process.env.PASSWORD}`)
 })
