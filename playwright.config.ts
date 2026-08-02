4771b132320f28977294ff175fc889529fa34166
mkrishna70 <mallam.krishna70@gmail.com>
2026-08-02 03:31:11 -0400

    committed

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html',{open:'always'}],['alluri-playwright']],
+  reporter: [['html',{open:'always'}],['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
